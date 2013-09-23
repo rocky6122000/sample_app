6a07cbabc1732885a0774dbe5d84b5c701a180c1
Hiroki Kamobayashi <rocky6122000@yahoo.co.jp>
2013-09-23 19:57:59 +0900

    change unit test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,41 +3,46 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home pages" do
-    it "should have the content 'sample App'" do
+    it "should have the h1 content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :test => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :test => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "should have the h1 content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :test => "Ruby on Rails Tutorial Sample App | About")
+                                :text => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
